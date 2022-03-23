b8fdd1a9cef7532c93417b15c213721f4ae540b8
Szymon <szymonzywko@gmail.com>
2022-03-23 20:43:14 +0300

    Update Readme.md

--- src/main/java/com/assigment/suretime/event/Event.java
+++ src/main/java/com/assigment/suretime/event/Event.java
@@ -1,15 +1,12 @@
 package com.assigment.suretime.event;
 
 import com.assigment.suretime.person.Person;
-import heat.Heat;
-import jdk.jfr.Percentage;
+import com.assigment.suretime.heat.Heat;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 
 @Data
 @Document
