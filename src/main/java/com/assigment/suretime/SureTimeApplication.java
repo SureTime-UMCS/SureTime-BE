61c5b7f3993c75a2c6647f318248eb204d182a8d
Szymon Sala <ssala@9bits.pl>
2022-04-16 17:39:47 +0200

    Revert "microservices"
    This reverts commit 3875b23879d46e1e3a85715be8f5cd28416825ce.

--- src/main/java/com/assigment/suretime/SureTimeApplication.java
+++ src/main/java/com/assigment/suretime/SureTimeApplication.java
@@ -2,10 +2,8 @@ package com.assigment.suretime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class SureTimeApplication {
 
     public static void main(String[] args) {
