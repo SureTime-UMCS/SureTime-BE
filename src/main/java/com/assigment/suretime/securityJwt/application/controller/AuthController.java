db80d1c009ebd48015224aff09d50518145db914
Pitucha Aleksander <63605795+PituchaAleksander@users.noreply.github.com>
2022-06-04 18:38:22 +0200

    refactor

--- src/main/java/com/assigment/suretime/securityJwt/application/controller/AuthController.java
+++ src/main/java/com/assigment/suretime/securityJwt/application/controller/AuthController.java
@@ -107,33 +107,6 @@ public class AuthController {
 				.orElseGet(()-> roleRepository.insert(new Role(ERole.ROLE_BASIC_USER)));
 		roles.add(role);
 
-//		if (strRoles == null) {
-//			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//			roles.add(userRole);
-//		} else {
-//			strRoles.forEach(role -> {
-//				switch (role) {
-//				case "admin":
-//					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-//							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//					roles.add(adminRole);
-//
-//					break;
-//				case "mod":
-//					Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-//							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//					roles.add(modRole);
-//
-//					break;
-//				default:
-//					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-//							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-//					roles.add(userRole);
-//				}
-//			});
-//		}
-
 		user.setRoles(roles);
 		userRepository.save(user);
 
